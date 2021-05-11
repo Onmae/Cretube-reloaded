40a0d419b91519e57af1052b28a01af334d7f9cf
Onmae <37141155+Onmae@users.noreply.github.com>
2021-05-11 23:02:30 +0900

    Add Router

--- src/server.js
+++ src/server.js
@@ -1,33 +1,17 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter"; 
 
-const PORT = 5000;
+const PORT = 4000;
 
 const app = express();
-
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-}
-
-const privateMiddleware = (req,res,next) => {
-    const url = req.url;
-    if(url === "/protected") {
-        return res.send("<h1>Not Allowed</h1>");
-    }
-    next(); 
-}
-
-const handleHome = (req, res,next) => {
-    res.end();
-};
-
-const handleProtected = (req, res) => {
-    return res.send("Welcome to the private lounge");
-}
+const logger = morgan("dev");
 
 app.use(logger);
-app.use(privateMiddleware);
-app.get("/",handleHome);
-app.get("/protected", handleProtected);
+app.use("/",globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
-app.listen(5000,() => console.log(`Server listening on port http://localhost:${PORT}`));
+app.listen(PORT,() => console.log(`Server listening on port http://localhost:${PORT}`));
